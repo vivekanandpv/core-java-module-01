8f56fe8b5125eb88d6033f2842c260603f06fd4e
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-23 11:29:17 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //  Comparison operators (>, >=, <, <=, ==, !=) return boolean results
+        int a = 14;
+        int b = 15;
+        double d = 3.25;
+
+        boolean result = a > b;
+
+        //  Boolean values can be operated with logical operators
+        //  || (or), && (and), ! (not)
+        //  Please note no other operators are defined for boolean values
+
+        boolean logicalResult = a > b || a != b;
+
+        //  Please explore comparison and logical operators
     }
 }
