a9537fb625e39496601d3f01c8e92f25e5922482
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-23 07:02:54 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //  Except for keywords such as public, static, void, int, etc.
+        //  you will see many different "name"s. In this file we can see
+        //  Main (line 1, name of the class), main (line 2, name of the method)
+        //  String (line 2, name of the type), args (line 2, name of the parameter)
+        //  System (line 3, name of the type), out (line 2, name of the field/class variable)
+        //  and println (line 3, name of the method)
+
+        //  Names in Java should follow the standard naming conventions:
+        //  Cannot have whitespaces (space, tab, newline), cannot start with a number,
+        //  cannot have special characters (though _ and $ are allowed, we abstain
+        //  from using them nonetheless).
+
+        //  There are different styles when it comes to naming in programming
+        //  PascalCase: EnjoyJavaProgramming (names of types: class, interface, enum, annotation)
+        //  camelCase: enjoyJavaProgramming (members of a class, local variables, and parameters)
+        //  snake_case: enjoy_java_programming (not used in Java, but popular in Python and C++ programming)
+        //  ALL_UPPER_CASE: ENJOY_JAVA_PROGRAMMING (for constants)
+
+        //  "Hello world!" is called a (string) "literal". This is the value that is literally written
+        //  without a name (identifier). Other examples are 1245 (integer literal), false (boolean literal), etc.
     }
 }
